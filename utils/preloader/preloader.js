cbc5e5a4935d57e9162718694b4f50af2ef79f90
rqaaattt <rqaaattt@gmail.com>
2025-04-20 12:47:59 +0500

    nineth-B3 commit

---
+++ utils/preloader/preloader.js
@@ -0,0 +1,16 @@
+// window.addEventListener('load', function () {
+//     const preloader = document.getElementById('preloader');
+//     setTimeout(() => {
+//       if (preloader) {
+//         preloader.style.display = 'none';
+//       }
+//     }, 10000);
+//   });
+  
+window.addEventListener('load', function () {
+  const preloader = document.getElementById('preloader');
+  if (preloader) {
+    preloader.style.display = 'none';
+  }
+});
+  
